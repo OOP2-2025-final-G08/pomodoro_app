198fa896c4687fd6e60db8c7fdf6e4f33c4d0eb8
raimu7 <k24089kk@aitech.ac.jp>
2026-01-08 16:02:41 +0900

    エラー解消

--- pomodoro_app/app.py
+++ pomodoro_app/app.py
@@ -16,18 +16,7 @@ def index():
     # 関数名は何でも良いですが、分かりやすく index に変更しました
     return render_template('index.html')
 
-# --- 【追加】タイマーページ ---
-@app.route('/timer')
-def timer():
-    # これにより /timer というURLにアクセスすると timer.html が表示されます
-    return render_template('timer.html')
 
-@app.route('/timer')
-def timer():
-    # URLの後ろの ?sets=4 を受け取る。なければ 1 にする
-    sets = request.args.get('sets', default=1, type=int)
-    # HTML側に sets という名前でデータを送る
-    return render_template('timer.html', sets=sets)
 
 if __name__ == '__main__':
     # host='0.0.0.0' は外部からの接続を許可する設定です
